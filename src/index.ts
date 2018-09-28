66e707895c92bc64c41f5c9c1472a6ab65da7eec
tobewhatwewant <tobewhatwewant@gmail.com>
2018-09-29 00:46:31 +0800

    feat(type guards): support type guards, with `value is T`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export interface IS {
-  (value: any): boolean;
-}
+/**
+ * Type guards (http://www.typescriptlang.org/docs/handbook/advanced-types.html)
+ */
 
 export type Primitive = null | undefined | string | number | boolean | Symbol;
 
@@ -28,33 +28,33 @@ const PrimitiveTypes = new Set([
 ]);
 
 // Primitive
-export const undefined: IS = value => typeof value === TypeName.undefined;
+export const undefined = (value: any): value is undefined => typeof value === TypeName.undefined;
 
-export const string: IS = value => typeof value === TypeName.string;
+export const string = (value: any): value is string=> typeof value === TypeName.string;
 
-export const number: IS = value => typeof value === TypeName.number;
+export const number = (value: any): value is number => typeof value === TypeName.number;
 
-export const boolean: IS = value => typeof value === TypeName.boolean;
+export const boolean = (value: any): value is boolean => typeof value === TypeName.boolean;
 
-export const symbol: IS = value => typeof value === TypeName.symbol;
+export const symbol = (value: any): value is symbol => typeof value === TypeName.symbol;
 
 // Functional
-export const object: IS = (value): value is object =>
+export const object = (value: any): value is object =>
   value !== null
   && (typeof value === TypeName.object || typeof value === TypeName.function);
 
-export const array: IS = value => Array.isArray(value);
+export const array = (value: any): value is Array<any> => Array.isArray(value);
 
-export const primitive = (value): value is Primitive => value === null || PrimitiveTypes.has(typeof value);
+export const primitive = (value: any): value is Primitive => value === null || PrimitiveTypes.has(typeof value);
 
-export const promise = (value): value is Promise<any> =>
+export const promise = (value: any): value is Promise<any> =>
   Object.prototype.toString.call(value) === '[object Promise]'
   || (!!value
       && (typeof value === TypeName.object || typeof value === TypeName.function)
       && typeof value.then === TypeName.function);
 
 // Integer
-export const integer = (value): value is number => Number.isInteger(value);
+export const integer = (value: any): value is number => Number.isInteger(value);
 
-export const safeInteger = (value): value is number => Number.isSafeInteger(value);
+export const safeInteger = (value: any): value is number => Number.isSafeInteger(value);
 
