92c586060a1704e2899902f58576fc2c095896a3
zero <tobewhatwewant@gmail.com>
2018-12-17 14:49:20 +0800

    :lipstics:(RegExp): add comments for is.regExp

--- src/types/regExp.ts
+++ src/types/regExp.ts
@@ -1,4 +1,8 @@
 import { TypeName } from '../utils/constants';
 import { isObjectOfType } from '../utils/util';
 
+/**
+ * is::regExp
+ * @param value RegExp
+ */
 export const regExp = isObjectOfType<RegExp>(TypeName.RegExp);
